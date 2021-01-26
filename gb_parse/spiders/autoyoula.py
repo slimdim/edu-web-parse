1eed3ff963c8f91e1d7cdca0d072881ed2b9a43e
dfilippov <dmitr.filippov@gmail.com>
2021-01-26 22:28:54 +0200

    save to db + dealers links

--- gb_parse/spiders/autoyoula.py
+++ gb_parse/spiders/autoyoula.py
@@ -1,5 +1,7 @@
 import scrapy
 import re
+from urllib.parse import urljoin
+from urllib.parse import unquote
 
 
 class AutoyoulaSpider(scrapy.Spider):
@@ -24,6 +26,10 @@ class AutoyoulaSpider(scrapy.Spider):
         'specifications': lambda resp: AutoyoulaSpider.get_specifications(resp)
     }
 
+    def __init__(self, pymongo_db, *args, **kwargs):
+        self.pymongo_db = pymongo_db
+        super().__init__(*args, **kwargs)
+
     def parse(self, response, **kwargs):
         brands_links = response.css(self.css_query['brands'])
         yield from self.gen_task(response, brands_links, self.brand_parse)
@@ -41,23 +47,34 @@ class AutoyoulaSpider(scrapy.Spider):
                 data[key] = selector(response)
             except (ValueError, AttributeError):
                 continue
-        print(1)
+        self.save(data)
+
+    def save(self, data):
+        collection = self.pymongo_db['youla']
+        collection.insert_one(data)
+        print(f'Object {data.get("title")} was saved')
 
     @staticmethod
     def get_author_url(response):
         script = response.css('script') \
             .re_first(r'(?<=<script>window.transitState = decodeURIComponent\(").*(?="\);</script>)')
-        user_id = re.search('youlaId%22%2C%22([0-9|a-zA-Z]+)%22%2C%22avatar', script)
+        encoded_script = unquote(script)
+        user_id = re.search('youlaId","([0-9|a-zA-Z]+)","avatar', encoded_script)
         if user_id:
-            return AutoyoulaSpider.user_url + str(user_id.group(1))
+            return urljoin(AutoyoulaSpider.user_url, user_id.group(1))
         else:
-            return None
+            dealer_link = re.search('sellerLink","([\/a-zA-Z\-0-9]+)","type', encoded_script)
+            if dealer_link:
+                return urljoin(AutoyoulaSpider.start_urls[0], dealer_link.group(1))
+            else:
+                return None
 
     @staticmethod
     def get_specifications(response):
         data = {}
-        spec_keys = response.css('div.AdvertCard_specs__2FEHc div.AdvertSpecs_label__2JHnS::text')
-        spec_values = response.css('div.AdvertCard_specs__2FEHc div.AdvertSpecs_data__xK2Qx::text, div.AdvertCard_specs__2FEHc a::text')
+        spec_keys = response.css('div.AdvertCard_specs__2FEHc div.AdvertSpecs_data__xK2Qx::attr(data-target)')
+        spec_values = response.css(
+            'div.AdvertCard_specs__2FEHc div.AdvertSpecs_data__xK2Qx::text, div.AdvertCard_specs__2FEHc a::text')
         for key, value in zip(spec_keys, spec_values):
             data[key.get()] = value.get()
         return data
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 from gb_parse.spiders.autoyoula import AutoyoulaSpider
+import dotenv
+import pymongo as pm
+import os
 
 if __name__ == '__main__':
+    pymongo_db = pm.MongoClient(os.getenv('MONGO_DB'))['gb_parse_01']
     crawler_settings = Settings()
     crawler_settings.setmodule('gb_parse.settings')
     crawler_process = CrawlerProcess(settings=crawler_settings)
-    crawler_process.crawl(AutoyoulaSpider)
+    crawler_process.crawl(AutoyoulaSpider,
+                          pymongo_db=pymongo_db)
     crawler_process.start()
